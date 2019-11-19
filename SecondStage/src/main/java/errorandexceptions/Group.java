7aa094e8c2310b1c7bdd5ea410b3393c20046965
Alexander Gritsok <agashayasha@gmail.com>
2019-11-19 14:51:57 +0300

    Added random students initialization in Group

--- SecondStage/src/main/java/errorandexceptions/Group.java
+++ SecondStage/src/main/java/errorandexceptions/Group.java
@@ -1,7 +1,30 @@
 package errorandexceptions;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Group {
+    private int id;
     private List<Student> students;
+    private List<Subject> subjects;
+    private Faculty faculty;
+    private static Random random = new Random();
+
+    public Group(int id, List<Subject> subjects, Faculty faculty) {
+        this.id = id;
+        this.students = initializeStudents();
+        this.subjects = subjects;
+        this.faculty = faculty;
+    }
+
+    public ArrayList<Student> initializeStudents() {
+        ArrayList<Student> studentsOfThisGroup = new ArrayList<Student>();
+        int numberOfStudents = random.nextInt(11);
+        for (int i = 0; i < numberOfStudents; i++) {
+            studentsOfThisGroup.add(new Student(this));
+        }
+        return studentsOfThisGroup;
+    }
+
 }
