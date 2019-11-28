2b8a89f6c0ecd15d59ce05022ec522e100194f9f
Alexander Gritsok <agashayasha@gmail.com>
2019-11-28 17:18:10 +0300

    Changed the method to make it universal.

--- SecondStage/module21/src/main/java/optionaltasks/FirstTask.java
+++ SecondStage/module21/src/main/java/optionaltasks/FirstTask.java
@@ -4,7 +4,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.Random;
+import java.util.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,14 +13,24 @@ public class FirstTask {
     static Logger logger = Logger.getLogger(FirstTask.class.getName());
 
     public static void main(String[] args) {
-        writeRandomNumbersToFile();
+        List<Integer> numbers = getRandomNumbers();
+        writeRandomNumbersToFile(numbers);
     }
 
-    private static void writeRandomNumbersToFile() {
+    private static ArrayList<Integer> getRandomNumbers() {
+        ArrayList<Integer> numbers = new ArrayList<>();
         int numbersAmount = random.nextInt(50) + 1;
+        for (int i = 0; i < numbersAmount; i++) {
+            numbers.add(random.nextInt(100));
+        }
+        return numbers;
+    }
+
+    private static void writeRandomNumbersToFile(Collection numbers) {
         try (PrintWriter writer = new PrintWriter(new BufferedWriter(new FileWriter("OptionalTask1.txt")))) {
-            for (int i = 0; i < numbersAmount; i++) {
-                writer.println(random.nextInt(100));
+            Iterator iterator = numbers.iterator();
+            while (iterator.hasNext()) {
+                writer.println(iterator.next());
             }
         } catch (IOException e) {
             logger.log(Level.SEVERE, e.getMessage());
