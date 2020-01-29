123d0c99cd7d8479dea11579ded7c9b9f8d98771
Alexander Gritsok <agashayasha@gmail.com>
2020-01-29 15:55:12 +0300

    Added log4j 1.2.17 since it's much easier to add custom appender at that version.

---
+++ taf/src/main/java/com/epam/tat/framework/logger/ReporterAppender.java
@@ -0,0 +1,91 @@
+package com.epam.tat.framework.logger;
+
+import org.apache.logging.log4j.core.*;
+import org.apache.logging.log4j.core.appender.AppenderLoggingException;
+import org.apache.logging.log4j.core.config.plugins.Plugin;
+import org.apache.logging.log4j.core.config.plugins.PluginAttribute;
+import org.apache.logging.log4j.core.config.plugins.PluginElement;
+import org.apache.logging.log4j.core.config.plugins.PluginFactory;
+import org.apache.logging.log4j.core.layout.PatternLayout;
+import org.testng.Reporter;
+
+import java.io.Serializable;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+@Plugin(name = "MyCustomAppender", category = "Core", elementType = "appender", printObject = true)
+public class ReporterAppender implements Appender {
+
+    private final ReadWriteLock rwLock = new ReentrantReadWriteLock();
+    private final Lock readLock = rwLock.readLock();
+
+    @Override
+    public void append(LogEvent logEvent) {
+        readLock.lock();
+        try {
+            Reporter.log(logEvent.getMessage().getFormattedMessage(), true);
+        } catch (Exception ex) {
+            if (!ignoreExceptions()) {
+                throw new AppenderLoggingException(ex);
+            }
+        } finally {
+            readLock.unlock();
+        }
+    }
+
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public Layout<? extends Serializable> getLayout() {
+        return null;
+    }
+
+    @Override
+    public boolean ignoreExceptions() {
+        return false;
+    }
+
+    @Override
+    public ErrorHandler getHandler() {
+        return null;
+    }
+
+    @Override
+    public void setHandler(ErrorHandler errorHandler) {
+
+    }
+
+    @Override
+    public State getState() {
+        return null;
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public boolean isStarted() {
+        return false;
+    }
+
+    @Override
+    public boolean isStopped() {
+        return false;
+    }
+}
