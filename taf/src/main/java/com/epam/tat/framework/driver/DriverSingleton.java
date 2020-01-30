60f6f37b9819e29cab1828c4c2daba7f97ed4473
Alexander Gritsok <agashayasha@gmail.com>
2020-01-30 13:41:58 +0300

    WebDriverManager logic is back and working. Firefox opens but geckodriver does not setups correctly even though it is downloaded.

--- taf/src/main/java/com/epam/tat/framework/driver/DriverSingleton.java
+++ taf/src/main/java/com/epam/tat/framework/driver/DriverSingleton.java
@@ -3,6 +3,7 @@ package com.epam.tat.framework.driver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverSingleton {
@@ -11,18 +12,18 @@ public class DriverSingleton {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            WebDriverManager.chromedriver().setup();
-            driver = new ChromeDriver();
-//            switch (System.getProperty("browser")) {
-//                case "firefox": {
-//                    WebDriverManager.firefoxdriver().setup();
-//                    driver = new FirefoxDriver();
-//                }
-//                default: {
-//                    WebDriverManager.chromedriver().setup();
-//                    driver = new ChromeDriver();
-//                }
-//            }
+            switch (System.getProperty("browser")) {
+                case "firefox": {
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                }
+                default: {
+                    WebDriverManager.chromedriver().setup();
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("--ignore-certificate-errors");
+                    driver = new ChromeDriver(options);
+                }
+            }
             driver.manage().window().maximize();
         }
         return driver;
