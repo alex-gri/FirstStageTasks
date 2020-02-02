ca9724c7c684208a25898ec519da27e8fdec9281
Alexander Gritsok <agashayasha@gmail.com>
2020-02-02 16:33:48 +0300

    Minor refactoring

--- taf/src/main/java/com/epam/tat/framework/runner/TestRunner.java
+++ taf/src/main/java/com/epam/tat/framework/runner/TestRunner.java
@@ -5,15 +5,16 @@ import com.beust.jcommander.ParameterException;
 import com.epam.tat.framework.listener.SuiteListener;
 import com.epam.tat.framework.listener.TestListener;
 import com.epam.tat.framework.logger.Log;
+import com.epam.tat.framework.util.StringUtils;
 import org.testng.TestNG;
 
 public class TestRunner {
 
     public static void main(String[] args) {
         parseCli(args);
-        Log.info("Starting app");
+        Log.logAndReport("Starting app with parameters: " + StringUtils.removeCommas(args));
         createTestNG().run();
-        Log.info("App finished");
+        Log.logAndReport("App finished");
     }
 
     private static TestNG createTestNG() {
@@ -22,7 +23,7 @@ public class TestRunner {
         testNG.addListener(new TestListener());
         testNG.setTestSuites(Arguments.instance().getSuites());
         testNG.setParallel(Arguments.instance().getParallel());
-        testNG.setThreadCount(Arguments.instance().getThreads());
+        testNG.setThreadCount(Arguments.instance().getThreadCount());
         return testNG;
     }
 
@@ -35,7 +36,6 @@ public class TestRunner {
                     .addObject(Arguments.instance())
                     .build()
                     .parse();
-            System.out.println(Arguments.instance().getSuites());
         } catch (ParameterException e) {
             Log.error(e.getMessage());
             jCommander.usage();
