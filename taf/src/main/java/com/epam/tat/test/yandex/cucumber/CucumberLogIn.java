2acae77c4611c4d5216b069ccc9b83a497b3539b
Alexander Gritsok <agashayasha@gmail.com>
2020-02-08 11:47:57 +0300

    Cucumber TestNG runner is reworked and moved to another package.

---
+++ taf/src/main/java/com/epam/tat/test/yandex/cucumber/CucumberLogIn.java
@@ -0,0 +1,28 @@
+package com.epam.tat.test.yandex.cucumber;
+
+import com.epam.tat.framework.ui.Browser;
+import io.cucumber.testng.*;
+import org.testng.annotations.AfterClass;
+
+@CucumberOptions(
+        strict = true,
+        features = {"src/main/resources/features"},
+        glue = {"com.epam.tat.test.yandex.cucumber"},
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/cucumber-pretty",
+                "json:target/cucumber-reports/CucumberTestReport.json",
+        })
+
+public class CucumberLogIn extends AbstractTestNGCucumberTests {
+
+
+    /*
+     * Browser sets up automatically at first call of instance.
+     * But we have to stop it manually here after all tests of class.
+     */
+    @AfterClass
+    public void tearDownBrowser() {
+        Browser.getInstance().stopBrowser();
+    }
+}
