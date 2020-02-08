ece247c51c4451ebea32b22a30bfea577a0b3880
Alexander Gritsok <agashayasha@gmail.com>
2020-02-09 00:11:23 +0300

    Added new feature and step definitions.

--- taf/src/main/java/com/epam/tat/test/yandex/cucumber/LogInStepDefinitions.java
+++ taf/src/main/java/com/epam/tat/test/yandex/cucumber/LogInStepDefinitions.java
@@ -1,14 +1,18 @@
 package com.epam.tat.test.yandex.cucumber;
 
+import com.epam.tat.framework.logger.Log;
 import com.epam.tat.framework.model.Account;
 import com.epam.tat.framework.model.builder.AccountBuilder;
+import com.epam.tat.framework.ui.Browser;
 import com.epam.tat.test.Constants;
 import com.epam.tat.yandex.disk.page.authorization.PassportYandexAuthorizationPage;
 import com.epam.tat.yandex.disk.page.home.YandexDiskHomePage;
+import io.cucumber.java.After;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.testng.Assert;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -17,6 +21,7 @@ import static org.hamcrest.Matchers.is;
 public class LogInStepDefinitions {
 
     private Account testAccount;
+    private PassportYandexAuthorizationPage authorizationPage;
 
     @Given("user is on home page")
     public void userIsOnHomePage() {
@@ -31,32 +36,64 @@ public class LogInStepDefinitions {
     @When("user clicks log in")
     public void userClicksLogIn() {
         new YandexDiskHomePage().logInButtonClick();
+        authorizationPage = new PassportYandexAuthorizationPage();
     }
 
     @When("user clicks submit")
     public void userClicksSubmit() {
-        new PassportYandexAuthorizationPage().logInButtonClick();
+        authorizationPage.logInButtonClick();
     }
 
     @And("user enters login")
     public void userEntersLogin() {
-        new PassportYandexAuthorizationPage().setLogin(testAccount.getLogin());
+
+        // Removing cookies from authorization page and refreshing it (login can be saved from previous tests).
+        Browser.getInstance().getWrappedDriver().manage().deleteAllCookies();
+        Browser.getInstance().getWrappedDriver().navigate().refresh();
+        Log.info("Cookies of current domain have been removed");
+
+        authorizationPage.setLogin(testAccount.getLogin());
     }
 
     @And("user enters password")
     public void userEntersPassword() {
-        new PassportYandexAuthorizationPage().setPassword(testAccount.getPassword());
+        authorizationPage.setPassword(testAccount.getPassword());
     }
 
     @And("user clicks on his profile avatar")
     public void userClicksOnHisProfileAvatar() {
-        new PassportYandexAuthorizationPage().profileAvatarButtonClick();
+        authorizationPage.profileAvatarButtonClick();
     }
 
     @Then("user sees his login")
     public void userSeesHisLogin() {
-        String loggedInAccountLogin = new PassportYandexAuthorizationPage().getLoggedInAccountLogin();
+        String loggedInAccountLogin = authorizationPage.getLoggedInAccountLogin();
         assertThat("Logged in account's login doesn't match to the given test-account's login",
                 loggedInAccountLogin, is(equalTo(testAccount.getLogin())));
+    }
+
+    @And("user has invalid login")
+    public void userHasInvalidLogin() {
+        testAccount = new AccountBuilder().login(Constants.INCORRECT_VALUE).build();
+    }
+
+    @Then("user sees error message")
+    public void userSeesErrorMessage() {
+        boolean isLogInFailed = authorizationPage.isLogInErrorMessagePresent();
+        Assert.assertTrue(isLogInFailed, "Log in state is not 'failed'");
+    }
+
+    /*
+     * Removing cookies to destroy current yandex disk session so we can return to the homepage.
+     */
+    @After
+    public void cleanCookies() {
+        Browser.getInstance().getWrappedDriver().manage().deleteAllCookies();
+        Log.info("Cookies of current domain have been removed");
+    }
+
+    @And("user has valid login and invalid password")
+    public void userHasValidLoginAndInvalidPassword() {
+        testAccount = new AccountBuilder().login(Constants.LOGIN).password(Constants.INCORRECT_VALUE).build();
     }
 }
--- taf/src/main/resources/features/login.feature
+++ taf/src/main/resources/features/login.feature
@@ -12,3 +12,21 @@ Feature: Yandex disk log in
     And user clicks submit
     And user clicks on his profile avatar
     Then user sees his login
+
+  Scenario: Log in with invalid login
+    Given user is on home page
+    And user has invalid login
+    When user clicks log in
+    And user enters login
+    And user clicks submit
+    Then user sees error message
+
+  Scenario: Log in with invalid password
+    Given user is on home page
+    And user has valid login and invalid password
+    When user clicks log in
+    And user enters login
+    And user clicks submit
+    And user enters password
+    And user clicks submit
+    Then user sees error message
