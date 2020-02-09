c4ff4ae5c12a64ef798d58d6be4990439ed1ec59
Alexander Gritsok <agashayasha@gmail.com>
2020-02-09 22:07:47 +0300

    Added one more feature and step definitions to it.

--- taf/src/main/java/com/epam/tat/test/yandex/cucumber/disk/DocumentStepDefinitions.java
+++ taf/src/main/java/com/epam/tat/test/yandex/cucumber/disk/DocumentStepDefinitions.java
@@ -4,10 +4,14 @@ import com.epam.tat.framework.model.Document;
 import com.epam.tat.framework.model.Folder;
 import com.epam.tat.framework.model.builder.DocumentBuilder;
 import com.epam.tat.framework.model.builder.FolderBuilder;
+import com.epam.tat.framework.runner.CucumberDisk;
+import com.epam.tat.yandex.disk.page.authorization.PassportYandexAuthorizationPage;
 import com.epam.tat.yandex.disk.page.createcontext.YandexDiskCreatePage;
+import com.epam.tat.yandex.disk.page.createdelement.YandexDiskFolderPage;
 import com.epam.tat.yandex.disk.page.createdelement.YandexDiskTextDocumentPage;
 import com.epam.tat.yandex.disk.page.service.DocumentService;
 import com.epam.tat.yandex.disk.page.service.FolderService;
+import io.cucumber.java.After;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -27,7 +31,7 @@ public class DocumentStepDefinitions {
         // It is already opened in BeforeClass
     }
 
-    @When("user creates folder with <random name>")
+    @When("user creates folder with <random folder name>")
     public void userCreatesFolderWithRandomName() {
         defaultTestFolder = new FolderBuilder().setDefaultName().build();
         FolderService.createFolder(defaultTestFolder);
@@ -38,9 +42,8 @@ public class DocumentStepDefinitions {
         new YandexDiskCreatePage().openCreatedFolder(defaultTestFolder);
     }
 
-    @And("clicks create text document")
+    @And("creates text document")
     public void clicksCreateTextDocument() {
-        defaultTestDocument = new DocumentBuilder().setDefaultName().setDefaultText().build();
         DocumentService.createDocument(defaultTestDocument);
     }
 
@@ -49,10 +52,46 @@ public class DocumentStepDefinitions {
         new YandexDiskTextDocumentPage().closeDocumentTab();
     }
 
-    @Then("user sees document inside folder with <random name>")
+    @Then("user sees document inside folder with <random folder name>")
     public void userSeesDocumentInsideFolderWithRandomName() {
-        String folderNameOfDocument = defaultTestFolder.getFolderPage().getContentTitle();
+        String folderNameOfDocument = new YandexDiskFolderPage().getContentTitle();
         assertThat("Name of the folder where document is doesn't match to the name of provided test-folded",
                 folderNameOfDocument, is(equalTo(defaultTestFolder.getName())));
+    }
+
+    @And("user types <text> into it")
+    public void userTypesTextIntoIt() {
+        new YandexDiskTextDocumentPage().writeToDocument(defaultTestDocument.getText());
+    }
+
+    @And("user has sample of document that consist of text and name")
+    public void userHasSampleOfDocumentThatIncludesTextAndName() {
+        defaultTestDocument = new DocumentBuilder().setDefaultName().setDefaultText().build();
+    }
+
+    @And("user sets <random document name>")
+    public void userSetsRandomDocumentName() {
+        DocumentService.setDocumentNameTo(defaultTestDocument);
+    }
+
+    @And("opens that document")
+    public void opensThatDocument() {
+        defaultTestFolder.getFolderPage().openDocument(defaultTestDocument);
+    }
+
+    @Then("user sees <text> that he typed earlier")
+    public void userSeesText() {
+        String documentText = defaultTestDocument.getDocumentPage().getDocumentText();
+        assertThat("Text in document after reopening doesn't match to the text given earlier at creating stage",
+                documentText, is(equalTo(defaultTestDocument.getText())));
+    }
+
+    @After
+    public void returnFilesPage() {
+        CucumberDisk.closeAllTabsExceptFirst();
+        new PassportYandexAuthorizationPage()
+                .openYandexDiskFilesPage()
+                .filesMenuItemClick()
+                .waitForContentTitleToBe("Файлы");
     }
 }
--- taf/src/main/resources/features/document.feature
+++ taf/src/main/resources/features/document.feature
@@ -2,8 +2,22 @@ Feature: Yandex disk text document
 
   Scenario: Document is created in correct folder
     Given user is on Files page
-    When user creates folder with <random name>
+    And user has sample of document that consist of text and name
+    When user creates folder with <random folder name>
     And opens that folder
-    And clicks create text document
+    And creates text document
+    And user sets <random document name>
     And closes that document
-    Then user sees document inside folder with <random name>
+    Then user sees document inside folder with <random folder name>
+
+  Scenario: Document is created, saved and reopened correctly
+    Given user is on Files page
+    And user has sample of document that consist of text and name
+    When user creates folder with <random folder name>
+    And opens that folder
+    And creates text document
+    And user types <text> into it
+    And user sets <random document name>
+    And closes that document
+    And opens that document
+    Then user sees <text> that he typed earlier
